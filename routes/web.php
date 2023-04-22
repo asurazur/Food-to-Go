67bfddeb341eb8ddb6772c948cd3706889dd111a
snacional <snacional@gbox.adnu.edu.ph>
2023-04-22 16:11:44 +0800

    edit

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/shop', function () { return view('shop');});
+Route::view('/menu', 'layouts.menu');
